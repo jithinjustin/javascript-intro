1f11fbbc9f7ff9c1bfe1030bffbb593196a4d68b
jithin <jithinjustin@gmail.com>
2020-08-24 15:25:51 -0400

    function parameters and defaults

--- src/index.js
+++ src/index.js
@@ -3,33 +3,15 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
-console.log(makeCar)
-console.log(shortHandArrow)
-//1 function declaration
-function makeCar(){
-    console.log('making car')
-}
+function makeCar(name='Porche!!!!'){ // default value
+   // name = name || 'Porche'
 
+    // if(!name){
+    //     name = 'Ferarri'
+    // }
+    console.log(`Making car: ${name.toUpperCase()}`)
+}
+makeCar('Porsche');
+
+makeCar("")
 makeCar()
-
-//2 function expression
-const carFunctionExpresson = function (){}
-console.log(carFunctionExpresson.name)
-
-//3 Arrow functions
-
-const carArrowFunctions = ()=>{
-    console.log(carArrowFunctions.name)
-}
-
-carArrowFunctions();
-// this behaves differently in a funciton declaration
-// and a arrow function
-
-//makearrow shorthand
-
-//implicit vs explicit return
-
-const shortHandArrow = ()=> console.log(shortHandArrow.name)
-
-shortHandArrow();
