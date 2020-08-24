6460ba8f04c87ad4ab422b191809c13ae1626676
jithin <jithinjustin@gmail.com>
2020-08-24 15:07:24 -0400

    intro to functions

--- src/index.js
+++ src/index.js
@@ -2,3 +2,34 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
+
+console.log(makeCar)
+console.log(shortHandArrow)
+//1 function declaration
+function makeCar(){
+    console.log('making car')
+}
+
+makeCar()
+
+//2 function expression
+const carFunctionExpresson = function (){}
+console.log(carFunctionExpresson.name)
+
+//3 Arrow functions
+
+const carArrowFunctions = ()=>{
+    console.log(carArrowFunctions.name)
+}
+
+carArrowFunctions();
+// this behaves differently in a funciton declaration
+// and a arrow function
+
+//makearrow shorthand
+
+//implicit vs explicit return
+
+const shortHandArrow = ()=> console.log(shortHandArrow.name)
+
+shortHandArrow();
