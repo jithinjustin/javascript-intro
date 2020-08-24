0a9db44c977787c4c1cb4083e496422891f4f0a3
jithin <jithinjustin@gmail.com>
2020-08-24 15:46:39 -0400

    arguments vs rest parameters

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
-function makeCar(name='Porche!!!!'){ // default value
-   // name = name || 'Porche'
+function makeCarPrice(){
+  //arguments object old way
+    console.log(arguments)
+    console.log(arguments[0])
+    console.log("iterating over arguments")
+    Array.from(arguments).forEach((a)=>console.log(a))
 
-    // if(!name){
-    //     name = 'Ferarri'
-    // }
-    console.log(`Making car: ${name.toUpperCase()}`)
+   const total = Array.from(arguments).reduce((prev, next)=> prev+next)
+  
+    console.log("total "+total)
+
 }
-makeCar('Porsche');
 
-makeCar("")
-makeCar()
+makeCarPrice(1,2,3);
+//rest parameter
+//at the end of list
+function makeCarPriceRest(number,...params){
+console.log(params)
+const total =params.reduce((prev, next)=>prev+next)
+console.log(`total ${total}`)
+}
+
+makeCarPriceRest(1,2,3);
