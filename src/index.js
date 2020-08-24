0cccfaa739ca5ce4a8e9678f0b74106dcb31f796
jithin <jithinjustin@gmail.com>
2020-08-24 17:13:35 -0400

    immediately invoked functions

--- src/index.js
+++ src/index.js
@@ -3,57 +3,20 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
-
-// //scope 1
-// const anotherid = "123abfda"
-
-// function makeCarPartid(id){
-// //scope 2
-
-// function anotherFunction(){
-//     //scope 3 look in scope then go up the chain
-//     const someId="adf";
-//     console.log(id,anotherid,someId)
-//     }
-// }
-
-// makeCarPartid('xdsafaf');
-// ///console.log(id);
-
-// function makeCarParId(id){
-//     const theId = `CAR_PART_${id}`
-//     return theId;
-// }
-
-// function makeCarParId(id){
-//     const theId = `CAR_PART_${id}`
-//     console.log(`create ${theId}`)
-//     return function(){
-//         console.log(theId);
-//     }
-// }
-
-// function makeCarParId(id){
-//     const theId = `CAR_PART_${id}`
-//     console.log(`create ${theId}`)
-//     return function(name){
-//         return `${theId}_${name.toUpperCase()}`
-//     }
-// }
-
-
-//arrow function
-
-function makeCarParId(id){
-    const theId = `CAR_PART_${id}`
-    console.log(`create ${theId}`)
-    return (name) => `${theId}_${name.toUpperCase()}`
-    
-}
-const carPartId = makeCarParId('affeaf');
-console.log(carPartId("LEFT DOOR") );
-console.log(carPartId("RIGHT DOOR") );
-
-const anothercarPartId = makeCarParId('faeafasf');
-console.log(anothercarPartId("LEFT DOOR") );
-console.log(anothercarPartId("RIGHT DOOR") );
+// ----
+// const carPartId = (function(id) {
+//     const theId = `CAR_PART_${id}`;
+//     return function(name) {
+//       return `${theId}_${name}`;
+//     };
+//   })('x7H8sFf');
+  
+const carPartId = (function(id) {
+    const theId = `CAR_PART_${id}`;
+    return (name) => `${theId}_${name}`; 
+  })('x7H8sFf');
+  console.log(carPartId)
+  console.log(carPartId('Left Door'));
+  console.log(carPartId('Right Door'));
+  console.log(carPartId('Windscreen'));
+  
