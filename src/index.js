eb02546092eaee45d7f9c75f1b244ad38ecff257
jithin <jithinjustin@gmail.com>
2020-08-24 17:45:59 -0400

    function and this

--- src/index.js
+++ src/index.js
@@ -2,27 +2,43 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
-
-
-// function carPart(name, fn){
-
-//     fn();
-
-
+// const firstCar = { id: 'x8KszK0' };
+// const secondCar = { id: 'bc90slqa' };
+// // example for this context based on callers context
+// function carPartId() {
+//   console.log(`${this.id}`);
 // }
 
-// carPart("hello",function(){
-//     console.log("I am callback");
-// })
-  
+// carPartId.call(firstCar)
+// carPartId.call(secondCar)
 
-function carPartId(name, fn) {
-    const theId = `CAR_PART_x8zOsl`;
-    return fn(`${theId}_${name}`);
-  }
-  
-  const carPart = carPartId('Left Door', function(id) {
-    return `Car Part ID: ${id}`;
-  });
-  
-  console.log(carPart);
+
+
+// const firstCar = { id: 'x8KszK0' };
+// const secondCar = { id: 'bc90slqa' };
+// // example for this context based on callers context
+// function carPartId(name,quantity) {
+//   console.log(`${this.id}_${name}_${quantity}`);
+// }
+// //call , c= commas
+// //apply, c= array
+// carPartId.call(firstCar,"Left Door",12)
+// carPartId.apply(secondCar,["Right Door",12])
+
+
+const firstCar = { id: 'x8KszK0' };
+const secondCar = { id: 'bc90slqa' };
+const thirdCar = { id: 'h9sNsa' };
+
+function carPartId(name, quantity) {
+  console.log(`${this.id}_${name}_${quantity}`);
+}
+
+const boundThirdCar = carPartId.bind(thirdCar);
+boundThirdCar('Windscreen', 99);
+boundThirdCar('Exhaust', 9);
+
+// call = c = commas
+carPartId.call(firstCar, 'Left Door', 12);
+// apply = a = array
+carPartId.apply(secondCar, ['Right Door', 21]);
